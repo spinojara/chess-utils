361e968188bb3a07483c95e5142ef3c15127ae15
Isak Ellmer <isak01@gmail.com>
2024-07-06 00:18:24 +0200

    Add nodecounter script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='chess-utils',
+        version='0.0a0',
+        scripts=['bin/nodecounter'],
+        packages=[],
+        author='Isak Ellmer',
+        author_email='isak01@gmail.com',
+        description='Random collection of chess utilities.',
+        url='https://github.com/spinojara/chess-utils',
+)
