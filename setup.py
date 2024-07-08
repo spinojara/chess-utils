1115d1f21304edb145f48ac1d23387b09e1c108c
Isak Ellmer <isak01@gmail.com>
2024-07-08 10:31:14 +0200

    Restructure package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
         name='chess-utils',
         version='0.0a0',
         scripts=['bin/nodecounter'],
-        packages=[],
+        packages=['nodecounter'],
         author='Isak Ellmer',
         author_email='isak01@gmail.com',
         description='A random collection of chess utilities.',
